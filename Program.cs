4bfcd8fb9b1cee44d513d29c115eb8511f07fbe8
Hywel Rees <hjr555@gmail.com>
2016-08-12 08:47:51 +0100

    Initial working version

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var y = DateTime.Now.Year;
+        var x = y - 120;
+
+        for(; y >= x; y--)
+        {
+            Console.Write($"{y}");
+            if(y != x) Console.Write(", ");
+        }
+    }
+}
